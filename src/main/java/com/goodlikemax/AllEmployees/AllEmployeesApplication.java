f6321f22879d4c840006bf5625bb2cd89aa5437e
goodlikemax <pidlisnyi.post@gmail.com>
2022-01-22 21:57:03 +0200

    Initial commit

---
+++ src/main/java/com/goodlikemax/AllEmployees/AllEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.goodlikemax.AllEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodlikemax/AllEmployees/AllEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodlikemax.AllEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
