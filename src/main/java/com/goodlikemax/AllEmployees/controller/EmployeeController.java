dc386a7b759fcc71c3435af35347b8bf169ddf81
goodlikemax <pidlisnyi.post@gmail.com>
2022-02-13 10:09:32 +0200

    frontend with react added

--- src/main/java/com/goodlikemax/AllEmployees/controller/EmployeeController.java
+++ src/main/java/com/goodlikemax/AllEmployees/controller/EmployeeController.java
@@ -3,7 +3,11 @@ package com.goodlikemax.AllEmployees.controller;
 import com.goodlikemax.AllEmployees.domain.Employee;
 import com.goodlikemax.AllEmployees.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * "Created by : goodlikemax"
@@ -12,31 +16,47 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("employees")
 public class EmployeeController {
-    @Autowired
+
     private EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @GetMapping
     public Iterable<Employee> list(){
         return employeeRepository.findAll();
     }
 
-    @GetMapping("{id}")
-    public Employee getById(@PathVariable long id){
-        return employeeRepository.findById(id);
+    @GetMapping("/{id}")
+    public Employee getById(@PathVariable Long id){
+        return employeeRepository.findById(id).orElseThrow(RuntimeException::new);
     }
-
 
 
     @PostMapping
-    public Employee create(@RequestBody Employee employee){
-        employeeRepository.save(employee);
-        return employee;
+    public ResponseEntity createEmployee(@RequestBody Employee employee) throws URISyntaxException {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.created(new URI("/employees/" + savedEmployee.getId())).body(savedEmployee);
     }
 
-    @PutMapping("{id}")
-    public Employee update(@RequestBody Employee employee){
-        employeeRepository.save(employee);
-        return employee;
+    @PutMapping("/{id}")
+    public ResponseEntity updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        Employee currentEmployee = employeeRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentEmployee.setFullName(employee.getFullName());
+        currentEmployee.setPhoneNumber(employee.getPhoneNumber());
+        currentEmployee.setSalary(employee.getSalary());
+        currentEmployee.setPosition(employee.getPosition());
+        currentEmployee = employeeRepository.save(employee);
+
+        return ResponseEntity.ok(employee);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteEmployee(@PathVariable Long id){
+        employeeRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 
--- src/main/java/com/goodlikemax/AllEmployees/repo/EmployeeRepository.java
+++ src/main/java/com/goodlikemax/AllEmployees/repo/EmployeeRepository.java
@@ -2,13 +2,14 @@ package com.goodlikemax.AllEmployees.repo;
 
 
 import com.goodlikemax.AllEmployees.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * "Created by : goodlikemax"
  * "Date: "23.01.2022
  */
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
   Employee findById(long id);
   Employee findByFullName(String fullName);
 
